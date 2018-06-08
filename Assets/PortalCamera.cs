3406cb77d2b1d4f351996097b915ebda23ed28fe
Jadw1 <jadw1_1@o2.pl>
2018-06-08 23:50:14 +0200

    not exactly working portal camera

---
+++ Assets/PortalCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+    public Transform portalA;
+    public Transform portalB;
+    private Transform playerCamera;
+
+	// Use this for initialization
+	void Start () {
+        playerCamera = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = playerCamera.position - portalA.position;
+        transform.position = portalB.position + offset;
+	}
+}
