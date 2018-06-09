f0dc5952c88e8b9acd6122aae76f1adc21fc5489
Jadw1 <jadw1_1@o2.pl>
2018-06-09 11:15:25 +0200

    some portal tests

---
+++ Assets/PortalManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PortalManager : MonoBehaviour {
+
+    public static PortalCamera cameraA;
+    public static PortalCamera cameraB;
+
+    private void Start() {
+        /*if(cameraA == null) {
+            Debug.LogError("No Camera A assigned!");
+        }
+
+        if (cameraB == null) {
+            Debug.LogError("No Camera B assigned!");
+        }*/
+        SetCameras();
+    }
+
+    private static void SetCameras() {
+        cameraA = GameObject.FindGameObjectWithTag("CameraA").GetComponent<PortalCamera>();
+        cameraB = GameObject.FindGameObjectWithTag("CameraB").GetComponent<PortalCamera>();
+    }
+
+    public static void CreatePortals(Transform portalA, Transform portalB) {
+
+
+        cameraA.cameraPortal = cameraB.oppositePortal = portalA;
+        cameraA.oppositePortal = cameraB.cameraPortal= portalB;
+        
+    }
+}
