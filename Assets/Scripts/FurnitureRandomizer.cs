e94fca8d5006f3f43f15cdb094ae9809144dcf61
Unknown <wsspad@gmail.com>
2018-06-10 00:03:27 +0200

    Room prefabs

---
+++ Assets/Scripts/FurnitureRandomizer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureRandomizer : MonoBehaviour {
+	public GameObject[] furniture;
+
+	private void Start() {
+		foreach (GameObject mebel in furniture) {
+			mebel.SetActive(Random.Range(0.0f, 100.0f) <= 25.0f ? true : false);
+		}
+	}
+}
