036918b7ceeb8bd8f70d1203e5c8d252327d5aaf
Unknown <wsspad@gmail.com>
2018-06-09 20:49:21 +0200

    Message system

---
+++ Assets/Scripts/MessageBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBox : MonoBehaviour {
+	public string[] messages;
+	public bool destroy = false;
+
+	private void OnTriggerEnter(Collider other) {
+		
+		foreach (string message in messages) {
+			TextMessage.AddMessage(message);
+		}
+
+		if (destroy) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/TextMessage.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextMessage : MonoBehaviour {
+
+	private static TextMessage INSTANCE = null;
+
+	private int typeSpeed = 5;
+	private int typeTimer = 0;
+	private int delayTicks = 0;
+
+	private bool typing = false;
+
+	private string message = "";
+
+	private Text text;
+
+	private Queue<string> queue = new Queue<string>();
+	private Queue<int> delayQueue = new Queue<int>();
+
+	public AudioClip[] keyStrokes;
+	public AudioSource audio;
+
+	private void Start() {
+		INSTANCE = this;
+
+		text = GetComponent<Text>();
+
+		text.text = "";
+
+		addMessage("Hi!");
+		addMessage("You go get that pizza.");
+		addMessage("It's at the end of this hallway.");
+		addMessage("Don't worry, you have only one way to go.");
+		addMessage("Just follow the only path.");
+		addMessage("You MIGHT want to remember where you go though...");
+	}
+
+
+	private void FixedUpdate() {
+		typeTimer++;
+
+		if (typeTimer > typeSpeed) {
+			typeTimer -= typeSpeed;
+
+			if (message.Length > 0) {
+				text.text += message[0];
+				message = message.Remove(0, 1);
+
+				// Play sound here.
+				audio.PlayOneShot(keyStrokes[Random.Range(0, keyStrokes.Length)]);
+
+			} else if (delayTicks > 0) {
+				delayTicks--;
+			} else {
+				typing = false;
+			}
+		}
+
+		if (!typing && queue.Count > 0) {
+			setMessage(queue.Dequeue());
+			delayTicks = delayQueue.Dequeue();
+		} else if (!typing) {
+			text.text = "";
+		}
+	}
+
+	public void setMessage(string message) {
+		this.message = message;
+		text.text = "";
+		typing = true;
+	}
+
+	public void addMessage(string message) {
+		addMessage(message, 10);
+	}
+
+	public void addMessage(string message, int delay) {
+		this.queue.Enqueue(message);
+		this.delayQueue.Enqueue(delay);
+	}
+
+	public bool isTyping() {
+		return typing;
+	}
+
+	public static void AddMessage(string message, int delay) {
+		if (INSTANCE != null) INSTANCE.addMessage(message, delay);
+	}
+
+	public static void AddMessage(string message) {
+		if (INSTANCE != null) INSTANCE.addMessage(message);
+	}
+}
