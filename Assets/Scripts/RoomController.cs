188a1087358b08adc3fd89eea94d47b420ea0b3c
Jadw1 <jadw1_1@o2.pl>
2018-06-10 00:32:14 +0200

    cleaning assets

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -7,5 +7,26 @@ public class RoomController : MonoBehaviour {
 
     private void Start() {
         doors = transform.GetComponentsInChildren<Door>();
+        if(gameObject.tag != "StartRoom" && gameObject.tag != "EndRoom") {
+            entryDoor = Random.Range(0, doors.Length);
+            exitDoor = Random.Range(0, doors.Length);
+            while(entryDoor == exitDoor) {
+                exitDoor = Random.Range(0, doors.Length);
+            }
+        }
+        else {
+            entryDoor = exitDoor = 0;
+        }
+    }
+
+    public void DeactivateDoors() {
+        Debug.Log("Deactivating");
+        for(int i = 0; i < doors.Length; i++) {
+            if (i == entryDoor || i == exitDoor) {
+                continue;
+            }
+
+            doors[i].gameObject.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/RoomTower.cs
+++ Assets/Scripts/RoomTower.cs
@@ -3,32 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomTower : MonoBehaviour {
-    /*public GameObject roomStarting;
-    public GameObject roomEnding;
+    public GameObject startRoom;
+    public GameObject endRoom;
     public GameObject[] roomArray;
-
     public int layers = 8;
 
-    public GameObject[] rooms;
+
+    private GameObject[] rooms;
 
     // create starting room, create layers, create ending room
 
     private void Start() {
         rooms = new GameObject[layers + 2];
 
-        rooms[0] = Instantiate(roomStarting, transform, false);
+        rooms[0] = Instantiate(startRoom, transform, false);
 
         for (int i = 1; i <= layers; i++) {
-            rooms[i] = Instantiate(roomArray[(int) Random.Range(0, roomArray.Length)], transform, false);
-
-			foreach (DoorTeleporter door in rooms[i].GetComponent<RoomController>().doors) {
-				door.gameObject.SetActive(false);
-			}
+            rooms[i] = Instantiate(roomArray[Random.Range(0, roomArray.Length)], transform, false);
+            rooms[i].GetComponent<RoomController>().DeactivateDoors();			
 		}
 
-        rooms[layers + 1] = Instantiate(roomEnding, transform, false);
+        rooms[layers + 1] = Instantiate(endRoom, transform, false);
 
-		GameObject pizza = GameObject.FindWithTag("Pizza");
+        for(int i = 0; i < rooms.Length; i++) {
+            rooms[i].transform.localPosition = new Vector3(0.0f, 5.0f * i, 0.0f);
+        }
+
+		/*GameObject pizza = GameObject.FindWithTag("Pizza");
 		if (pizza != null) {
 			pizza.GetComponent<PizzaLogic>().tower = this;
 		}
@@ -59,10 +60,10 @@ public class RoomTower : MonoBehaviour {
 				entrance.link = exit;
 				exit.link = entrance;
 			}
-		}
+		}*/
     }
 
-	public void pizzaTaken() {
+	/*public void pizzaTaken() {
 		DoorTeleporter pizza = rooms[rooms.Length - 1].GetComponent<RoomController>().doors[0];
 
 		foreach (GameObject room in rooms) {
