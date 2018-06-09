9581259de12b8f751bba5145fb5171bb87e764e7
Unknown <wsspad@gmail.com>
2018-06-09 22:37:37 +0200

    "Music" update.

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+	public AudioClip[] clips;
+
+	private AudioSource audio;
+
+	private void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+
+	private void Update() {
+		if (!audio.isPlaying) {
+			audio.clip = clips[Random.Range(0, clips.Length)];
+			audio.Play();
+		}
+	}
+}
