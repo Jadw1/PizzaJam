aaff0f68f44c4bde3f9e9ead624a35354e451093
Unknown <wsspad@gmail.com>
2018-06-10 09:25:59 +0200

    Added room indicators

---
+++ Assets/Scripts/RoomIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomIndicator : MonoBehaviour {
+	private Text text;
+	private RectTransform parent;
+	public RoomController room;
+
+	private void Start() {
+		text = GetComponent<Text>();
+
+		text.text = "Floor " + room.roomIndex;
+
+		parent = GetComponentInParent<RectTransform>();
+	}
+
+	private void Update() {
+		parent.Rotate(new Vector3(0, Time.deltaTime * -100, 0));
+	}
+}
