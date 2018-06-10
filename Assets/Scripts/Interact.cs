ec8e1126e8790aaf53bec66a36b31bfe4d676be0
Unknown <wsspad@gmail.com>
2018-06-10 15:44:00 +0200

    FIXED SECRET CODE

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -9,14 +9,28 @@ public class Interact : MonoBehaviour {
 	public Text interactText;
 
 	public int[] code = new int[6];
-	int curCode = 0;
 	
     private void Start() {
         camera = Camera.main;
     }
 
 	private bool isCodeCorrect() {
-		return code[0] == 1 && code[1] == 1 && code[2] == 1 && code[3] == 0 && code[4] == 0 && code[5] == 1;
+		return code[0] == 1 && code[1] == 0 && code[2] == 0 && code[3] == 1 && code[4] == 1 && code[5] == 1;
+	}
+
+	private void resetCode() {
+		for (int i = 0; i < code.Length; i++) {
+			code[i] = 0;
+		}
+	}
+
+	private void inputCode(int c) {
+		code[5] = code[4];
+		code[4] = code[3];
+		code[3] = code[2];
+		code[2] = code[1];
+		code[1] = code[0];
+		code[0] = c;
 	}
 
     void Update () {
@@ -27,7 +41,12 @@ public class Interact : MonoBehaviour {
 		if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)) {
 			if (Input.GetKeyDown(KeyCode.E)) {
 				if (hit.transform.tag == "Door") {
-					hit.transform.GetComponent<Door>().Open(isCodeCorrect() ? true : false);
+					if (isCodeCorrect()) {
+						hit.transform.GetComponent<Door>().Open(true);
+						resetCode();
+					} else {
+						hit.transform.GetComponent<Door>().Open(false);
+					}
 				}
 			}
 
@@ -43,13 +62,11 @@ public class Interact : MonoBehaviour {
 				}
 			} else if (hit.transform.tag == "LeftLamp") {
 				if (Input.GetKeyDown(KeyCode.E)) {
-					code[curCode++] = 1;
-					if (curCode == code.Length) curCode = 0;
+					inputCode(1);
 				}
 			} else if (hit.transform.tag == "RightLamp") {
 				if (Input.GetKeyDown(KeyCode.E)) {
-					code[curCode++] = 0;
-					if (curCode == code.Length) curCode = 0;
+					inputCode(0);
 				}
 			}
 		}
